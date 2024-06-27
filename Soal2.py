fcf909a6e59571f1a0858c1bc66ed469c256a3a1
Poxezy <136463325+Poxezy@users.noreply.github.com>
2024-06-27 23:19:42 +0700

    Add files via upload

---
+++ Soal2.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+dataset = pd.read_csv('d:/MACHINELEARNING/Dataset.csv', sep=';')
+
+numerical_columns = ['R&D Spend', 'Administration', 'Marketing Spend', 'Profit']
+numerical_data = dataset[numerical_columns]
+
+X = numerical_data.iloc[:, :-1].values
+y = numerical_data.iloc[:, -1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+print(regressor.coef_)
+print(regressor.intercept_)
