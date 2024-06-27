c47b801668686982ce7b9f38133a881deecea59e
Poxezy <136463325+Poxezy@users.noreply.github.com>
2024-06-28 00:14:39 +0700

    Soal2.py

--- Soal2.py
+++ Soal2.py
@@ -1,23 +1,29 @@
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-dataset = pd.read_csv('d:/MACHINELEARNING/Dataset.csv', sep=';')
 
-numerical_columns = ['R&D Spend', 'Administration', 'Marketing Spend', 'Profit']
-numerical_data = dataset[numerical_columns]
+dataset = pd.read_csv('D:/machinelearning/Dataset.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
 
-X = numerical_data.iloc[:, :-1].values
-y = numerical_data.iloc[:, -1].values
+dataset.head()
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
 y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2)
+
 
 print(regressor.coef_)
 print(regressor.intercept_)
